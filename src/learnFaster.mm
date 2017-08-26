c7338b5c9b9cbceacaf2325add81fefa214e512c
Israel Roldan <israel.alberto.rv@gmail.com>
2017-08-26 17:57:41 -0400

    Added the mind maps file with the init map.

---
+++ src/learnFaster.mm
@@ -0,0 +1,18 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1503783974447" ID="ID_538468957" MODIFIED="1503783991956" TEXT="Learn faster">
+<node CREATED="1503784013409" ID="ID_1389720821" MODIFIED="1503784342739" POSITION="right" TEXT="Read">
+<node CREATED="1503784224311" ID="ID_1263429104" MODIFIED="1503784481092" TEXT="Active reading">
+<font NAME="SansSerif" SIZE="12"/>
+<node CREATED="1503784145707" ID="ID_1686471021" MODIFIED="1503784289246" TEXT="Concentration"/>
+<node CREATED="1503784048150" ID="ID_879199492" MODIFIED="1503784092441" TEXT="Convert words in concepts"/>
+<node CREATED="1503784150242" ID="ID_34133726" MODIFIED="1503784154173" TEXT="Convert in facts"/>
+<node CREATED="1503784154888" ID="ID_1400501093" MODIFIED="1503784325440" TEXT="Create movies in our head"/>
+</node>
+<node CREATED="1503784344866" ID="ID_1110938694" MODIFIED="1503784381556" TEXT="Passive reading">
+<node CREATED="1503784408343" ID="ID_1820280688" MODIFIED="1503784451417" TEXT="Not happens the cognitive process"/>
+<node CREATED="1503784490600" ID="ID_814882844" MODIFIED="1503784578144" TEXT="Only repeats the words into the book"/>
+</node>
+</node>
+</node>
+</map>
