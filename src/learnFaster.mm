5b1499b5edf8e50b4b0bf37af8c98195c2c90dc3
Israel Roldan <israel.alberto.rv@gmail.com>
2017-08-27 01:10:08 -0400

    Added stickers until the the video #19 in video course: Learning How to Learn From Video Courses

--- src/learnFaster.mm
+++ src/learnFaster.mm
@@ -1,18 +1,217 @@
 <map version="1.0.1">
 <!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
-<node CREATED="1503783974447" ID="ID_538468957" MODIFIED="1503783991956" TEXT="Learn faster">
-<node CREATED="1503784013409" ID="ID_1389720821" MODIFIED="1503784342739" POSITION="right" TEXT="Read">
-<node CREATED="1503784224311" ID="ID_1263429104" MODIFIED="1503784481092" TEXT="Active reading">
+<node COLOR="#000000" CREATED="1503783974447" ID="ID_538468957" MODIFIED="1503790688060" STYLE="fork" TEXT="Learning">
+<edge COLOR="#808080" STYLE="bezier"/>
+<font NAME="SansSerif" SIZE="20"/>
+<hook NAME="accessories/plugins/AutomaticLayout.properties"/>
+<node COLOR="#0033ff" CREATED="1503790403731" ID="ID_288941237" MODIFIED="1503790716840" POSITION="right" TEXT="Conventional learning">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1503788650226" ID="ID_1237087481" MODIFIED="1503790370352" TEXT="Requirements">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1503788656096" ID="ID_1884501764" MODIFIED="1503790370352" TEXT="Feel pressured">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="14"/>
+<node COLOR="#111111" CREATED="1503788745402" ID="ID_138542054" MODIFIED="1503790370353" TEXT="Pay attention">
 <font NAME="SansSerif" SIZE="12"/>
-<node CREATED="1503784145707" ID="ID_1686471021" MODIFIED="1503784289246" TEXT="Concentration"/>
-<node CREATED="1503784048150" ID="ID_879199492" MODIFIED="1503784092441" TEXT="Convert words in concepts"/>
-<node CREATED="1503784150242" ID="ID_34133726" MODIFIED="1503784154173" TEXT="Convert in facts"/>
-<node CREATED="1503784154888" ID="ID_1400501093" MODIFIED="1503784325440" TEXT="Create movies in our head"/>
 </node>
-<node CREATED="1503784344866" ID="ID_1110938694" MODIFIED="1503784381556" TEXT="Passive reading">
-<node CREATED="1503784408343" ID="ID_1820280688" MODIFIED="1503784451417" TEXT="Not happens the cognitive process"/>
-<node CREATED="1503784490600" ID="ID_814882844" MODIFIED="1503784578144" TEXT="Only repeats the words into the book"/>
+<node COLOR="#111111" CREATED="1503789011041" ID="ID_1380798017" MODIFIED="1503790370353" TEXT="Only have one chance">
+<font NAME="SansSerif" SIZE="12"/>
 </node>
+<node COLOR="#111111" CREATED="1503789018989" ID="ID_1613559988" MODIFIED="1503790370354" TEXT="You cannot repeat at anytime">
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node COLOR="#111111" CREATED="1503788814069" ID="ID_559134564" MODIFIED="1503801096870" TEXT="When other people see you">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="12"/>
+<node COLOR="#111111" CREATED="1503788840612" ID="ID_175075771" MODIFIED="1503801181238" TEXT="You does your best effort or pretending that">
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node COLOR="#111111" CREATED="1503788878771" ID="ID_1756997988" MODIFIED="1503790362920" TEXT="Finish the goal">
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+</node>
+</node>
+<node COLOR="#990000" CREATED="1503793080759" ID="ID_491780589" MODIFIED="1503793969950" TEXT="Get something value for yourself">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+<node COLOR="#00b439" CREATED="1503801781214" ID="ID_1098914393" MODIFIED="1503801785707" TEXT="Focused mode">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1503801790649" ID="ID_1973984869" MODIFIED="1503801827005" TEXT="3B / Relax your brain">
+<font NAME="SansSerif" SIZE="14"/>
+<node COLOR="#111111" CREATED="1503801800590" ID="ID_262914172" MODIFIED="1503802364617" TEXT="Bed: Take a rest"/>
+<node COLOR="#111111" CREATED="1503801870926" ID="ID_1795451072" MODIFIED="1503801880565" TEXT="Bath: Take a shower"/>
+<node COLOR="#111111" CREATED="1503801886601" ID="ID_1450465537" MODIFIED="1503801941424" TEXT="Bus: Move to another location (cafeteria, pool, living room)"/>
+</node>
+<node COLOR="#990000" CREATED="1503802235836" ID="ID_341637981" MODIFIED="1503802260424" TEXT="How to active the focuse mode">
+<font NAME="SansSerif" SIZE="14"/>
+<node COLOR="#111111" CREATED="1503802261438" ID="ID_1319869961" MODIFIED="1503802336791" TEXT="Body position: sit down, straight back, feet in the floor"/>
+<node COLOR="#111111" CREATED="1503802414407" ID="ID_1601187493" MODIFIED="1503802440066" TEXT="Not distractions: music, social media, e-mail"/>
+</node>
+<node COLOR="#990000" CREATED="1503809391198" ID="ID_71080194" MODIFIED="1503809439051" TEXT="When the subject does not appassionated you">
+<font NAME="SansSerif" SIZE="14"/>
+<node COLOR="#111111" CREATED="1503809449917" ID="ID_285465917" MODIFIED="1503809570996" TEXT="1. We need to learn other subjects to accomplish your goals / dreams"/>
+<node COLOR="#111111" CREATED="1503809571555" ID="ID_274907721" MODIFIED="1503809631021" TEXT="2. Change your thought -I hate this- for -it&apos;s hard but I need it-"/>
+<node COLOR="#111111" CREATED="1503809678499" ID="ID_324834255" MODIFIED="1503809708144" TEXT="3. Got interesting in go deep">
+<node COLOR="#111111" CREATED="1503809817464" ID="ID_708664615" MODIFIED="1503809830931" TEXT="Read the content of the subject"/>
+<node COLOR="#111111" CREATED="1503809831542" ID="ID_1682314489" MODIFIED="1503809851716" TEXT="Ask you questions about some specific terms or sub-subjects"/>
+</node>
+<node COLOR="#111111" CREATED="1503809885182" ID="ID_1968281709" MODIFIED="1503809916081" TEXT="4. Make sure you have the background knowledge">
+<node COLOR="#111111" CREATED="1503809987499" ID="ID_1779478684" MODIFIED="1503810034046" TEXT="Take notes and research after you finish this subject">
+<node COLOR="#111111" CREATED="1503810340881" ID="ID_1359600984" MODIFIED="1503810382603" TEXT="1. Requires immediate action"/>
+<node COLOR="#111111" CREATED="1503810383300" ID="ID_1310046991" MODIFIED="1503810405636" TEXT="2. Important things"/>
+<node COLOR="#111111" CREATED="1503810408177" ID="ID_933413955" MODIFIED="1503810415836" TEXT="3. Not important"/>
+</node>
+</node>
+</node>
+</node>
+<node COLOR="#00b439" CREATED="1503802752773" ID="ID_1110860824" MODIFIED="1503802755351" TEXT="Review">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1503802757001" ID="ID_349751117" MODIFIED="1503802816885" TEXT="Deep learning">
+<font NAME="SansSerif" SIZE="14"/>
+<node COLOR="#111111" CREATED="1503802818109" ID="ID_561048064" MODIFIED="1503803003518" TEXT="If you take note during the learning process then&#xa;it&apos;s important finished and do a different activity then&#xa;come back and write the notes those you wrote,&#xa;the notes you missing are the subjects you need to reforce"/>
+</node>
+</node>
+<node COLOR="#00b439" CREATED="1503793467025" ID="ID_662294296" MODIFIED="1503793855578" TEXT="Weakness">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1503793276063" ID="ID_1802747330" MODIFIED="1503793300417" TEXT="Cognitive illusion of knowing">
+<font NAME="SansSerif" SIZE="14"/>
+<node COLOR="#111111" CREATED="1503793874193" ID="ID_785744677" MODIFIED="1503794209867" TEXT="Problem: Keep goint next, next">
+<node COLOR="#111111" CREATED="1503794215421" ID="ID_560902926" MODIFIED="1503794250288" TEXT="I understand at the moment but then I don&apos;t remember anything"/>
+<node COLOR="#111111" CREATED="1503793892048" ID="ID_1680955032" MODIFIED="1503794329543" TEXT="Solution: Calibrate the knowledge">
+<node COLOR="#111111" CREATED="1503794310346" ID="ID_1017170331" MODIFIED="1503794408422" TEXT="Stop, reflect, ask yourself questions then write all the ideas those are important for yourself"/>
+<node COLOR="#111111" CREATED="1503794505993" ID="ID_1150377802" MODIFIED="1503794510985" TEXT="Developing skills that are right for you"/>
+</node>
+</node>
+</node>
+</node>
+<node COLOR="#00b439" CREATED="1503789287326" ID="ID_626115393" MODIFIED="1503793456098" TEXT="Unknown knowledge">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1503789318570" ID="ID_1782400835" MODIFIED="1503793456099" TEXT="1. Get a general context">
+<font NAME="SansSerif" SIZE="14"/>
+<node COLOR="#111111" CREATED="1503789333810" ID="ID_1039012662" MODIFIED="1503790362920" TEXT="Easy explanation in comprehensive concepts"/>
+</node>
+<node COLOR="#990000" CREATED="1503790745493" ID="ID_5900057" MODIFIED="1503793456100" TEXT="2. Get basic ideas">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1503790843545" ID="ID_496038694" MODIFIED="1503801029278" TEXT="s">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1503790793988" ID="ID_931516848" MODIFIED="1503793456101" TEXT="4. Goes deep in the sub-ideas">
+<font NAME="SansSerif" SIZE="14"/>
+<node COLOR="#111111" CREATED="1503790894910" ID="ID_1057108819" MODIFIED="1503790900044" TEXT="Repeat the step 1"/>
+</node>
+</node>
+<node COLOR="#00b439" CREATED="1503790429423" ID="ID_1516096396" MODIFIED="1503790528381" TEXT="Top-bottom approach to education">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1503794538638" ID="ID_1558082236" MODIFIED="1503801014317" TEXT="The teacher decide what is&#xa;the best learning for you">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+<node COLOR="#00b439" CREATED="1503784013409" ID="ID_1389720821" MODIFIED="1503790692511" STYLE="fork" TEXT="Read">
+<edge COLOR="#808080" STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1503784224311" ID="ID_1263429104" MODIFIED="1503801469956" STYLE="fork" TEXT="Active reading /&#xa;Focused mode">
+<edge COLOR="#808080" STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="14"/>
+<node COLOR="#111111" CREATED="1503784746567" ID="ID_1340682113" MODIFIED="1503790692512" STYLE="fork" TEXT="Skills">
+<edge COLOR="#808080" STYLE="bezier"/>
+<font NAME="SansSerif" SIZE="12"/>
+<node COLOR="#111111" CREATED="1503784145707" ID="ID_1686471021" MODIFIED="1503787074722" STYLE="fork" TEXT="Concentration">
+<edge COLOR="#808080" STYLE="bezier"/>
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node COLOR="#111111" CREATED="1503784048150" ID="ID_879199492" MODIFIED="1503787074723" STYLE="fork" TEXT="Convert words in concepts">
+<edge COLOR="#808080" STYLE="bezier"/>
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node COLOR="#111111" CREATED="1503784150242" ID="ID_34133726" MODIFIED="1503787074723" STYLE="fork" TEXT="Convert in facts">
+<edge COLOR="#808080" STYLE="bezier"/>
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node COLOR="#111111" CREATED="1503784154888" ID="ID_1400501093" MODIFIED="1503787074724" STYLE="fork" TEXT="Create movies in our head">
+<edge COLOR="#808080" STYLE="bezier"/>
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+</node>
+<node COLOR="#111111" CREATED="1503784753076" ID="ID_1689244704" MODIFIED="1503790692513" STYLE="fork" TEXT="Needs">
+<edge COLOR="#808080" STYLE="bezier"/>
+<font NAME="SansSerif" SIZE="12"/>
+<node COLOR="#111111" CREATED="1503784762161" ID="ID_998167922" MODIFIED="1503787559302" STYLE="fork" TEXT="Create a main">
+<edge COLOR="#808080" STYLE="bezier"/>
+<font NAME="SansSerif" SIZE="12"/>
+<node COLOR="#111111" CREATED="1503787443353" ID="ID_1469166739" MODIFIED="1503787574376" TEXT="Create the structures for this idea">
+<node COLOR="#111111" CREATED="1503787291104" ID="ID_599814064" MODIFIED="1503787516804" TEXT="Different concepts">
+<node COLOR="#111111" CREATED="1503784857140" ID="ID_1598045811" MODIFIED="1503787607578" STYLE="fork" TEXT="Determine the value of each concept">
+<edge COLOR="#808080" STYLE="bezier"/>
+<font NAME="SansSerif" SIZE="12"/>
+<node COLOR="#111111" CREATED="1503784963751" ID="ID_1426607868" MODIFIED="1503787074727" STYLE="fork" TEXT="Superior concept">
+<edge COLOR="#808080" STYLE="bezier"/>
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node COLOR="#111111" CREATED="1503786431859" ID="ID_392767231" MODIFIED="1503787074727" STYLE="fork" TEXT="Inferior concept">
+<edge COLOR="#808080" STYLE="bezier"/>
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node COLOR="#111111" CREATED="1503786456754" ID="ID_1664802544" MODIFIED="1503787074728" STYLE="fork" TEXT="Examples">
+<edge COLOR="#808080" STYLE="bezier"/>
+<font NAME="SansSerif" SIZE="12"/>
+<node COLOR="#111111" CREATED="1503786461533" ID="ID_894478171" MODIFIED="1503787074728" STYLE="fork" TEXT="Heading is a 150 valued idea">
+<edge COLOR="#808080" STYLE="bezier"/>
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node COLOR="#111111" CREATED="1503786473229" ID="ID_1023730280" MODIFIED="1503787074729" STYLE="fork" TEXT="Sub heading is a 120 valued idea">
+<edge COLOR="#808080" STYLE="bezier"/>
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node COLOR="#111111" CREATED="1503786511832" ID="ID_305743916" MODIFIED="1503787074730" STYLE="fork" TEXT="Sub sub heading is a 100 valued idea">
+<edge COLOR="#808080" STYLE="bezier"/>
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node COLOR="#111111" CREATED="1503786522643" ID="ID_1091745603" MODIFIED="1503787074730" STYLE="fork" TEXT="First paragraph is a 60 valued idea">
+<edge COLOR="#808080" STYLE="bezier"/>
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node COLOR="#111111" CREATED="1503786574184" ID="ID_235076514" MODIFIED="1503787074731" STYLE="fork" TEXT="Italics and bolds add 10 valued idea">
+<edge COLOR="#808080" STYLE="bezier"/>
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+</node>
+</node>
+</node>
+</node>
+</node>
+</node>
+<node COLOR="#111111" CREATED="1503788037461" ID="ID_1859411349" MODIFIED="1503790692514" TEXT="Interrump and reactivate the reading">
+<font NAME="SansSerif" SIZE="12"/>
+<node COLOR="#111111" CREATED="1503788062692" ID="ID_14156755" MODIFIED="1503788358087" TEXT="You are able to stop the reading then&#xa;come back a few minutes, hours or days,&#xa;read 3 pages back and re-active the learning"/>
+</node>
+</node>
+<node COLOR="#990000" CREATED="1503784344866" ID="ID_1110938694" MODIFIED="1503801441600" STYLE="fork" TEXT="Passive reading /&#xa;Diffused mode">
+<edge COLOR="#808080" STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="14"/>
+<node COLOR="#111111" CREATED="1503784408343" ID="ID_1820280688" MODIFIED="1503790692516" STYLE="fork" TEXT="Not happens the cognitive process">
+<edge COLOR="#808080" STYLE="bezier"/>
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node COLOR="#111111" CREATED="1503784490600" ID="ID_814882844" MODIFIED="1503790692516" STYLE="fork" TEXT="Only repeats the words into the book">
+<edge COLOR="#808080" STYLE="bezier"/>
+<font NAME="SansSerif" SIZE="12"/>
+<node COLOR="#111111" CREATED="1503800871061" ID="ID_314278880" MODIFIED="1503800947928" TEXT="It&apos;s the same process when we watch a TV sport game,&#xa;we only follow the logic presented in the TV"/>
+</node>
+</node>
+</node>
+</node>
+<node COLOR="#0033ff" CREATED="1503800796416" ID="ID_405300585" MODIFIED="1503800801351" POSITION="right" TEXT="Fast learning">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
 </node>
 </node>
 </map>
